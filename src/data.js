21b3cc8f05387874c091d94e2ffe04f544ecb30e
Arfah Ali <arfahali994@gmail.com>
2023-06-22 23:03:30 +0500

    .

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
- const List = [
+const List = [
   {
     id: 1,
     image: "//ProductImages/product-5.jpg",
     name: "Grey Shoes",
     rating: 1,
-    price: 50.00,
+    price: 50.0,
     quantity: 1,
   },
   {
@@ -12,102 +12,91 @@
     image: "//ProductImages/product-6.jpg",
     name: "Black T-Shirt",
     rating: 3,
-    price: 60.00,
+    price: 60.0,
     quantity: 1,
-
   },
   {
     id: 3,
-    image: require("//ProductImages/product-7.jpg"),
+    image: "//ProductImages/product-7.jpg",
     name: "3-Pair of Sockes",
     rating: 4,
-    price: 30.00,
+    price: 30.0,
     quantity: 1,
-
   },
   {
     id: 4,
-    image: require("//ProductImages/product-8.jpg"),
+    image: "//ProductImages/product-8.jpg",
     name: "Fossil Watch",
     rating: 1,
-    price: 80.00,
+    price: 80.0,
     quantity: 1,
-
   },
   {
     id: 5,
-    image: require("//ProductImages/product-9.jpg"),
+    image: "//ProductImages/product-9.jpg",
     name: "Watch",
     rating: 3,
-    price: 80.00,
+    price: 80.0,
     quantity: 1,
-
   },
   {
     id: 6,
-    image: require("//ProductImages/product-10.jpg"),
+    image: "//ProductImages/product-10.jpg",
     name: "Black Sneakers",
     rating: 3,
-    price: 50.00,
+    price: 50.0,
     quantity: 1,
-
   },
   {
     id: 7,
-    image: require("//ProductImages/product-11.jpg"),
+    image: "//ProductImages/product-11.jpg",
     name: "Brown Trouser",
     rating: 3,
-    price: 60.00,
+    price: 60.0,
     quantity: 1,
-
   },
   {
     id: 8,
-    image: require("//ProductImages/product-12.jpg"),
+    image: "//ProductImages/product-12.jpg",
     name: "Comfy Shoes",
     rating: 5,
-    price: 70.00,
+    price: 70.0,
     quantity: 1,
-
   },
   {
     id: 9,
-    image: require("//ProductImages/product-5.jpg"),
+    image: "//ProductImages/product-5.jpg",
     name: "Red T-Shirt",
     rating: 1,
-    price: 50.00,
+    price: 50.0,
     quantity: 1,
-
   },
   {
     id: 10,
-    image: require("//ProductImages/product-6.jpg"),
+    image: "//ProductImages/product-6.jpg",
     name: "Grey Sneakers",
     rating: 3,
-    price: 40.00,
+    price: 40.0,
     quantity: 1,
-
   },
   {
     id: 11,
-    image: require("//ProductImages/product-7.jpg"),
+    image: "//ProductImages/product-7.jpg",
     name: "Socks",
     rating: 3,
-    price: 60.00,
+    price: 60.0,
     quantity: 1,
-
   },
   {
     id: 12,
-    image: require("//ProductImages/product-8.jpg"),
+    image: "//ProductImages/product-8.jpg",
     name: "Sneakers",
     rating: 3,
-    price: 70.00,
+    price: 70.0,
     quantity: 1,
-
   },
 ];
-const testimonials=[
+const testimonials = [
   {
     id: 1,
     quote:
@@ -134,5 +123,5 @@ const testimonials=[
   },
 ];
 
-const value= {List, testimonials};
+const value = { List, testimonials };
 export default value;
